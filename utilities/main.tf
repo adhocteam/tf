bcea0a3d6aa9d286639de7c0f722801c1f10b28d
Robert Fairhead <bob@adhocteam.us>
2018-10-08 14:24:39 -0500

    Make utilities individually toggable

--- utilities/main.tf
+++ utilities/main.tf
@@ -1,12 +1,19 @@
+locals {
+  teleport = "${var.ssh_bastion >=1 ? 1 : 0}"
+  jenkins  = "${var.jenkins >=1 ? 1 : 0}"
+}
+
 module "teleport" {
+  count             = "${local.teleport}"
   source            = "./teleport"
   region            = "${var.region}"
   env               = "${var.env}"
   domain_name       = "${var.domain_name}"
-  emergency_jumpbox = 1
+  emergency_jumpbox = "${var.jumpbox}"
 }
 
 module "jenkins" {
+  count       = "${local.jenkins}"
   source      = "./jenkins"
   env         = "${var.env}"
   domain_name = "${var.domain_name}"
