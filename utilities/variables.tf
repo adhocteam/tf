8739097770a85087a9ebd1ca813e3ad314cdf012
Robert Fairhead <bob@adhocteam.us>
2018-10-26 15:57:01 -0500

    revert variables

--- utilities/variables.tf
+++ utilities/variables.tf
@@ -5,18 +5,3 @@ variable "env" {
 variable "domain_name" {
   description = "the external domain name for reaching the public resources. must have a certificate in ACM associated with it."
 }
-
-variable "ssh_bastion" {
-  description = "Set to 0 (zero) to disable the ssh proxy bastion, Teleport"
-  default     = 1
-}
-
-variable "jumpbox" {
-  description = "Set to 1 (one) to enable the emergency SSH jumpbox host"
-  default     = 0
-}
-
-variable "jenkins" {
-  description = "Set to 0 (zero) to disable the local install of jenkins at jenksin.domain_name"
-  default     = 1
-}
