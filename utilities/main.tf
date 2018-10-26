455ca3af1fb0e1a90050b5f9fb29ce6fa9d9515d
Robert Fairhead <bob@adhocteam.us>
2018-10-26 15:55:15 -0500

    Revert utilities

--- utilities/main.tf
+++ utilities/main.tf
@@ -1,15 +1,18 @@
-### This is a convenience for installing the basic set of ulitities
-### To customize, just import the ones you want directly
-
 module "teleport" {
   source      = "./teleport"
-  region      = "${var.region}"
   env         = "${var.env}"
   domain_name = "${var.domain_name}"
 }
 
 module "jenkins" {
   source      = "./jenkins"
+  env         = "${var.env}"
+  domain_name = "${var.domain_name}"
+}
+
+# Disabled by default
+module "jumpbox" {
+  source      = "./jumpbox"
   env         = "${var.env}"
   domain_name = "${var.domain_name}"
 }
