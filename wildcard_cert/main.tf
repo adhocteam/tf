748c995097844c9e50392ac6af0491f788720513
Robert Fairhead <robertfairhead@users.noreply.github.com>
2018-10-26 22:45:04 -0500

    Try to fix multiple domains for validation (#13)

--- wildcard_cert/main.tf
+++ wildcard_cert/main.tf
@@ -25,9 +25,10 @@ resource "aws_acm_certificate_validation" "domain" {
 }
 
 resource "aws_route53_record" "validation" {
-  name    = "${aws_acm_certificate.domain.domain_validation_options.0.resource_record_name}"
-  type    = "${aws_acm_certificate.domain.domain_validation_options.0.resource_record_type}"
+  count   = "${length(aws_acm_certificate.domain.domain_validation_options)}"
+  name    = "${element(aws_acm_certificate.domain.domain_validation_options.*.resource_record_name, count.index)}"
+  type    = "${element(aws_acm_certificate.domain.domain_validation_options.*.resource_record_type, count.index)}"
   zone_id = "${data.aws_route53_zone.external.id}"
-  records = ["${aws_acm_certificate.domain.domain_validation_options.0.resource_record_value}"]
+  records = ["${element(aws_acm_certificate.domain.domain_validation_options.*.resource_record_value, count.index)}"]
   ttl     = 60
 }
