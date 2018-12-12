87dcc703e8db43469a0275d5c103694eff023c8f
Robert Fairhead <bob@adhocteam.us>
2018-12-11 20:00:16 -0600

    Fix security group for database

--- database/main.tf
+++ database/main.tf
@@ -14,11 +14,11 @@ resource "aws_security_group" "db_sg" {
 }
 
 resource "aws_security_group_rule" "app_ingress" {
-  type            = "ingress"
-  from_port       = 5432
-  to_port         = 5432
-  protocol        = "tcp"
-  security_groups = ["${var.app_sg}"]
+  type                     = "ingress"
+  from_port                = 5432
+  to_port                  = 5432
+  protocol                 = "tcp"
+  source_security_group_id = ["${var.app_sg}"]
 
   security_group_id = "${aws_security_group.db_sg.id}"
 }
