e5dad60eb7b0ea5c4469af18f285c35e7b3f5bef
Robert Fairhead <bob@adhocteam.us>
2019-02-06 12:29:04 -0600

    Add CPU-based autoscaling for ECS cluster

--- fargate_cluster/main.tf
+++ fargate_cluster/main.tf
@@ -135,3 +135,87 @@ resource "aws_iam_role_policy" "ecs_execution" {
 }
 EOF
 }
+
+######
+# Auto-scaling for ECS cluster
+######
+
+resource "aws_appautoscaling_target" "service" {
+  service_namespace  = "ecs"
+  resource_id        = "service/${aws_ecs_cluster.app.name}/${aws_ecs_service.application.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  max_capacity       = 8
+  min_capacity       = 2
+}
+
+resource "aws_cloudwatch_metric_alarm" "cpu_utilization_high" {
+  alarm_name          = "${var.env}-${var.application_name}-CPU-Utilization-High"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/ECS"
+  period              = "60"
+  statistic           = "Average"
+  threshold           = 80
+
+  dimensions {
+    ClusterName = "${aws_ecs_cluster.app.name}"
+    ServiceName = "${aws_ecs_service.application.name}"
+  }
+
+  alarm_actions = ["${aws_appautoscaling_policy.up.arn}"]
+}
+
+resource "aws_cloudwatch_metric_alarm" "cpu_utilization_low" {
+  alarm_name          = "${var.env}-${var.application_name}-CPU-Utilization-High"
+  comparison_operator = "LessThanThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/ECS"
+  period              = "60"
+  statistic           = "Average"
+  threshold           = 40
+
+  dimensions {
+    ClusterName = "${aws_ecs_cluster.app.name}"
+    ServiceName = "${aws_ecs_service.application.name}"
+  }
+
+  alarm_actions = ["${aws_appautoscaling_policy.down.arn}"]
+}
+
+resource "aws_appautoscaling_policy" "up" {
+  name               = "app-scale-up"
+  service_namespace  = "${aws_appautoscaling_target.service.service_namespace}"
+  resource_id        = "${aws_appautoscaling_target.service.resource_id}"
+  scalable_dimension = "${aws_appautoscaling_target.service.scalable_dimension}"
+
+  step_scaling_policy_configuration {
+    adjustment_type         = "ChangeInCapacity"
+    cooldown                = 60
+    metric_aggregation_type = "Average"
+
+    step_adjustment {
+      metric_interval_lower_bound = 0
+      scaling_adjustment          = 1
+    }
+  }
+}
+
+resource "aws_appautoscaling_policy" "down" {
+  name               = "app-scale-down"
+  service_namespace  = "${aws_appautoscaling_target.service.service_namespace}"
+  resource_id        = "${aws_appautoscaling_target.service.resource_id}"
+  scalable_dimension = "${aws_appautoscaling_target.service.scalable_dimension}"
+
+  step_scaling_policy_configuration {
+    adjustment_type         = "ChangeInCapacity"
+    cooldown                = 300
+    metric_aggregation_type = "Average"
+
+    step_adjustment {
+      metric_interval_upper_bound = 0
+      scaling_adjustment          = -1
+    }
+  }
+}
