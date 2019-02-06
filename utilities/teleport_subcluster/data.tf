25aef249bf06e4da95fbee9ba848133d69609ae3
Robert Fairhead <bob@adhocteam.us>
2019-02-06 16:45:54 -0600

    Remove unused secrets

--- utilities/teleport_subcluster/data.tf
+++ utilities/teleport_subcluster/data.tf
@@ -24,14 +24,6 @@ data "aws_kms_key" "main" {
   key_id = "alias/${var.env}-main"
 }
 
-data "aws_secretsmanager_secret_version" "github_client_id" {
-  secret_id = "${var.env}/teleport/github_client_id"
-}
-
-data "aws_secretsmanager_secret_version" "github_secret" {
-  secret_id = "${var.env}/teleport/github_secret"
-}
-
 data "aws_secretsmanager_secret" "cluster_token" {
   name = "${var.env}/teleport/cluster_token"
 }
