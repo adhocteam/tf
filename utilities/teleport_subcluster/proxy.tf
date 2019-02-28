122111337ce8d9d40cd298ecd7f942b259ebb5a1
Robert Fairhead <bob@adhocteam.us>
2019-02-27 21:16:30 -0600

    Fix merge conflict

--- utilities/teleport_subcluster/proxy.tf
+++ utilities/teleport_subcluster/proxy.tf
@@ -79,11 +79,7 @@ resource "aws_security_group_rule" "jumpbox_proxy" {
   from_port                = 22
   to_port                  = 22
   protocol                 = "tcp"
-<<<<<<< HEAD
-  source_security_group_id = "${var.jumpbox_sg}"
-=======
   source_security_group_id = "${data.aws_security_group.jumpbox.id}"
->>>>>>> va-demo
 
   security_group_id = "${aws_security_group.proxies.id}"
 }
