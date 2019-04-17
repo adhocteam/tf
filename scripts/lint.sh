47ce0504bee9326aeed43b8e94dcb729f6f504a7
Robert Fairhead <bob@adhocteam.us>
2019-04-17 13:07:03 -0500

    Fix linting script

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,10 +1,10 @@
 #! /usr/bin/env bash
 
-non_camel=$(find . -name "*.tf" -exec grep -l "^[a-z].*-" {} \;)
+non_camel=$(find . -name "*.tf" -exec grep -l "^(resource|data|output|variable).*-" {} \;)
 if [[ ! -z "$non_camel" ]]; then
     echo "Resources should be camel_case and not use hyphens:"
     for f in "$non_camel"; do
-        grep -n "^[a-z].*-" $f
+        grep -n "^(resource|data|output|variable).*-" $f
     done
     exit 1
 fi
