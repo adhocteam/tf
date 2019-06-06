1d0fbe91f2feb18dc711ff791e1277897e13ef3c
Robert Fairhead <bob@adhocteam.us>
2019-06-06 14:03:38 -0500

    Terraform 0.12upgrade of plain_instance

--- plain_instance/outputs.tf
+++ plain_instance/outputs.tf
@@ -1,8 +1,9 @@
 output "app_sg_id" {
-  value = "${module.base.app_sg_id}"
+  value = module.base.app_sg_id
 }
 
 output "instance_iam_role" {
   description = "IAM role name for attaching additional policies to the instance with aws_iam_role_policy_attachment"
-  value       = "${aws_iam_role.iam.name}"
+  value       = aws_iam_role.iam.name
 }
+
