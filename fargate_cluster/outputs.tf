89b98dd72b427afb0dae85d78a7cd7dc06d19eb5
Robert Fairhead <bob@adhocteam.us>
2019-07-23 19:34:22 -0500

    pass through docker image for use by console

--- fargate_cluster/outputs.tf
+++ fargate_cluster/outputs.tf
@@ -3,6 +3,11 @@ output "name" {
   value       = var.application_name
 }
 
+output "docker_image" {
+  description = "Images in the Docker Hub registry are available by default. You can also specify other repositories with either repository-url/image:tag or repository-url/image@digest"
+  value       = var.docker_image
+}
+
 output "security_group" {
   description = "object with the application-specific security group. defaults to unrestricted egress"
   value       = aws_security_group.fargate
