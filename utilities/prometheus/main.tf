bc929513a062f2b96ddd29cc8f2bc5a941b547b0
Robert Fairhead <bob@adhocteam.us>
2019-09-03 16:53:54 -0500

    fix ignoring of base AMI changes

--- utilities/prometheus/main.tf
+++ utilities/prometheus/main.tf
@@ -23,7 +23,7 @@ resource "aws_autoscaling_group" "prometheus" {
   wait_for_capacity_timeout = "600s"
 
   lifecycle {
-    ignore_changes        = [desired_capacity, ami]
+    ignore_changes        = [desired_capacity]
     create_before_destroy = true
   }
 
@@ -96,6 +96,11 @@ resource "aws_launch_template" "prometheus" {
 
   monitoring {
     enabled = true
+  }
+
+  # Ignore base AMI changes. Trigger deploys my manually tainting this
+  lifecycle {
+    ignore_changes = [image_id]
   }
 
   tags = {
