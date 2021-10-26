f20c1d9c2ac4fe49a1bf62ee5173933c795c6ef7
Brandon Cruz <brandon@adhocteam.us>
2021-10-26 13:26:45 -0400

    Update main.tf

--- database/main.tf
+++ database/main.tf
@@ -1,5 +1,8 @@
 terraform {
   required_version = ">= 0.12"
+  required_providers {
+    random = ">=3.0"
+  }
 }
 
 locals {
@@ -180,12 +183,6 @@ resource "aws_iam_role_policy_attachment" "monitoring" {
 # Create and Store Username/Password in secrets manager with rotation
 ##################################################
 
-provider "random" {
-  random = {
-      source = "hashicorp/random"
-      version = "3.1.0"
-    }
-}
 resource "random_password" "db_password" {
   length           = 20
   special          = true
