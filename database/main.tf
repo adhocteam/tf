fdb1b600c765956bff322b4f9ee54adc4f4ce9ba
Brandon Cruz <brandon@adhocteam.us>
2021-10-26 15:35:13 -0400

    Update main.tf

--- database/main.tf
+++ database/main.tf
@@ -180,9 +180,6 @@ resource "aws_iam_role_policy_attachment" "monitoring" {
 # Create and Store Username/Password in secrets manager with rotation
 ##################################################
 
-provider "random" {
-  version = ">= 3.0"
-}
 resource "random_password" "db_password" {
   length           = 20
   special          = true
